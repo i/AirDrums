1219de46360fa3389540dd63fa48727ab6a54ca1
Ian Lozinski <ian.lozinski@gmail.com>
2013-04-07 05:19:56 -0400

    added generic sound path

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
-var app = require('express')()
+var express = require('express')
+  , app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server);
 
 server.listen(3000);
 console.log('app running at 3000');
+app.use('/assets', express.static('assets'));
 
 app.get('/', function(req, res){
   res.sendfile(__dirname + '/index.html');
