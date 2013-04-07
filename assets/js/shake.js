3cd3052f6cf01f7cb3ab144f641db15aea3a5f08
Eddie Zaneski <eddiezane@gmail.com>
2013-04-07 10:35:20 -0400

    Change shake threshold

--- assets/js/shake.js
+++ assets/js/shake.js
@@ -16,7 +16,7 @@
 		this.hasDeviceMotion = 'ondevicemotion' in window;
 
 		//default velocity threshold for shake to register
-		this.threshold = 15;
+		this.threshold = 10;
 
 		//use date to prevent multiple shakes firing	
 		this.lastTime = new Date();
