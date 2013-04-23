fa6bf626e242febed324716c566aee0bd12ddacd
Eddie Zaneski <eddiezane@gmail.com>
2013-04-23 21:07:51 +0000

    Change pool to 5 and remove ogg

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,25 +1,12 @@
 var socket = io.connect('/');
 
-var snare = AudioFX('/assets/sounds/snare', { formats: ['ogg', 'mp3'], pool:10 } );
-//var snare = new Audio('/assets/sounds/snare.wav');
-
-var hihat = AudioFX('/assets/sounds/hihat', { formats: ['ogg', 'mp3'], pool:10  } );
-//var hihat = new Audio('/assets/sounds/hihat.wav');
-
-var cowbell = AudioFX('/assets/sounds/cowbell', { formats: ['ogg', 'mp3'], pool:10  } );
-//var cowbell = new Audio('/assets/sounds/cowbell.wav');
-
-var tom1 = AudioFX('/assets/sounds/tom1', { formats: ['ogg', 'mp3'], pool:10  } );
-//var tom1 = new Audio('/assets/sounds/tom1.wav');
-
-var tom2 = AudioFX('/assets/sounds/tom2', { formats: ['ogg', 'mp3'], pool:10  } );
-//var tom2 = new Audio('/assets/sounds/tom2.wav');
-
-var ride = AudioFX('/assets/sounds/ride', { formats: ['ogg', 'mp3'], pool:10  } );
-//var ride = new Audio('assets/sounds/ride.wav');
-
-var crash = AudioFX('/assets/sounds/crash', { formats: ['ogg', 'mp3'], pool:10  } );
-//var crash = new Audio('assets/sounds/crash.wav');
+var snare = AudioFX('/assets/sounds/snare', { formats: ['mp3'], pool: 5 } );
+var hihat = AudioFX('/assets/sounds/hihat', { formats: ['mp3'], pool: 5 } );
+var cowbell = AudioFX('/assets/sounds/cowbell', { formats: ['mp3'], pool: 5 } );
+var tom1 = AudioFX('/assets/sounds/tom1', { formats: ['mp3'], pool: 5 } );
+var tom2 = AudioFX('/assets/sounds/tom2', { formats: ['mp3'], pool: 5 } );
+var ride = AudioFX('/assets/sounds/ride', { formats: ['mp3'], pool: 5 } );
+var crash = AudioFX('/assets/sounds/crash', { formats: ['mp3'], pool: 5 } );
 
 socket.on('action', function(data){
   console.log(data);
