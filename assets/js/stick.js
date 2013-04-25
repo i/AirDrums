db315426ec8570bcd352fa86d0382e477e70c912
Eddie Zaneski <eddiezane@gmail.com>
2013-04-25 06:46:29 +0000

    Modify stick.html ui

--- assets/js/stick.js
+++ assets/js/stick.js
@@ -3,68 +3,68 @@ var socket = io.connect('/')
 , angle_offset = 0
 , iphone = navigator.userAgent.toLowerCase().indexOf("iphone");
 
-function sendHit(action)
-{
+function sendHit(action) {
   console.log(action);
   socket.emit('device-motion', action);
 };
 
 function joinSession(sessionID) {
   socket.emit('join', sessionID);
+  $('#sessionForm').hide();
 }
 
 window.addEventListener('shake', shaken, false);
 
-function shaken(){
+function shaken() {
   if (iphone === -1) {
-    if(angle <= 45){
+    if(angle <= 45) {
       sendHit('crash');
     }
-    if(angle > 45 && angle <= 90){
+    if(angle > 45 && angle <= 90) {
       sendHit('tom1');
     }
-    if(angle > 90 && angle <= 135){
+    if(angle > 90 && angle <= 135) {
       sendHit('tom2');
     }
-    if(angle > 135 && angle <= 225){
+    if(angle > 135 && angle <= 225) {
       sendHit('cowbell');
     }
-    if(angle > 225 && angle <= 270){
+    if(angle > 225 && angle <= 270) {
       sendHit('ride');
     }
-    if(angle > 270 && angle <= 315){
+    if(angle > 270 && angle <= 315) {
       sendHit('hihat');
     }
-    if(angle > 315){
+    if(angle > 315) {
       sendHit('snare');
     }
   }
   else {
-    if(angle <= 45){
+    if(angle <= 45) {
       sendHit('snare');
     }
-    if(angle > 45 && angle <= 90){
+    if(angle > 45 && angle <= 90) {
       sendHit('hihat');
     }
-    if(angle > 90 && angle <= 135){
+    if(angle > 90 && angle <= 135) {
       sendHit('ride');
     }
-    if(angle > 135 && angle <= 225){
+    if(angle > 135 && angle <= 225) {
       sendHit('cowbell');
     }
-    if(angle > 225 && angle <= 270){
+    if(angle > 225 && angle <= 270) {
       sendHit('tom2');
     }
-    if(angle > 270 && angle <= 315){
+    if(angle > 270 && angle <= 315) {
       sendHit('tom1');
     }
-    if(angle > 315){
+    if(angle > 315) {
       sendHit('crash');
     }
   }
 }
 
-$(document).on('vclick', "#kick", function(){
+$(document).on('vclick', "#kick", function() {
   shaken();
 });
 
