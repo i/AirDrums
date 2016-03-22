518be82a34d2b77e7b14744bb9e072b080fd084a
Eddie Zaneski <eddiezane@gmail.com>
2016-03-22 11:13:05 -0400

    Add start script and port for heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express')
   , app = express()
   , server = require('http').createServer(app)
   , io = require('socket.io').listen(server)
-  , port = 8080
+  , port = process.env.PORT || 8080
 
 
 server.listen(port);
